c4b7f49f5b21067b7cba3e86e5597e98264ea2b6
joshua7v <joshua7v@hotmail.com>
2021-01-15 21:38:59 +0800

    open a window

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 
 IF NOT EXIST .\build mkdir .\build
 pushd .\build
-cl -nologo -FC ../src/main.c -link User32.lib
+cl -nologo -Zi -FC ../src/main.c -link User32.lib
 popd
--- src/main.c
+++ src/main.c
@@ -1,6 +1,48 @@
 #include <windows.h>
 
+BOOL globalRunning = TRUE;
+
+LRESULT CALLBACK windowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    LRESULT result = 0;
+    switch (msg) {
+        case WM_CLOSE: {
+            globalRunning = FALSE;
+            PostQuitMessage(0);
+        } break;
+        default: {
+            result = DefWindowProc(hwnd, msg, wParam, lParam);
+        }
+    }
+
+    return result;
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
-    MessageBoxA(0, "Hello World", 0, MB_OK);
+
+    WNDCLASS windowClass = {0};
+    windowClass.lpszClassName = "ReGamesWindowClass";
+    windowClass.lpfnWndProc = windowProc;
+
+    RegisterClass(&windowClass);
+
+    CreateWindow(windowClass.lpszClassName,
+                 "ReGamesWindow",
+                 WS_VISIBLE | WS_OVERLAPPEDWINDOW,
+                 CW_USEDEFAULT,
+                 CW_USEDEFAULT,
+                 CW_USEDEFAULT,
+                 CW_USEDEFAULT,
+                 0,
+                 0,
+                 0,
+                 0);
+
+    while (globalRunning) {
+        MSG msg;
+        while (PeekMessage(&msg, 0, 0, 0, PM_REMOVE)) {
+            DispatchMessage(&msg);
+        }
+    }
+
     return 0;
 }
